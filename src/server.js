a6b4769338e1439e8d7ab0058a58bf3549d95373
meiraPe <p.meira@aluno.ifsp.edu.br>
2025-08-12 17:48:52 -0300

    criado o crud do profile

--- src/server.js
+++ src/server.js
@@ -2,8 +2,38 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json()) //Função usada para transformar os dados de string para objeto em JS
+
+app.post('/profile', (req, res) => {
+  const dados = req.body
+  console.log(dados)
+  
+  //criar dados do usuário
+  res.json({
+    message:'Perfil Criado Com Sucesso!',
+    profile: dados
+  })
+})
+
+app.get('/profile', (req, res) => {
+  //consultar dados do usuario
+  res.json({message: 'Perfil Consultado Com Sucesso!'})
+})
+
+app.put('/profile', (req, res) => {
+  const dados = req.body
+  console.log(dados)
+
+  //editar dados do usuário
+  res.json({
+    message: 'Perfil Editado Com Sucesso!',
+    profile: dados
+  })
+})
+
+app.delete('/profile', (req, res) => {
+  //deletar dados do usuário
+  res.json({message: 'Perfil Deletado Com Sucesso!'})
 })
 
 app.listen(port, () => {
